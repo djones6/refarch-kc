27d572a612153210e0ac20767fa70d9aefb9bb4f
David Jones <djones6@uk.ibm.com>
2020-01-21 14:22:28 +0000

    Add install for first microservice, fixes

--- install.bat
+++ install.bat
@@ -13,8 +13,12 @@ kubectl wait --for=condition=Ready kafkas/my-cluster -n kafka --timeout 180s
 :: Create namespace for Postgres
 kubectl create ns postgres
 
-:: TODO - is this only an OpenShift concern?
+:: Create namespace for refarch-kc microservices
+kubectl create ns shipping
+
+:: TODO - is this required outside of OpenShift?
 kubectl create serviceaccount -n postgres pgserviceaccount
+kubectl create serviceaccount -n shipping kcserviceaccount
 
 :: Install Postgres Helm chart
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
@@ -25,14 +29,19 @@ kubectl create secret generic postgresql-url --from-literal=binding="jdbc:postgr
 kubectl create secret generic postgresql-user --from-literal=binding="postgres" -n shipping
 kubectl create secret generic postgresql-pwd --from-literal=binding="supersecret" -n shipping
 
-:: Create namespace for refarch-kc microservices
-kubectl create ns shipping
-
 :: Create a configmap to allow microservices to find Kafka
-kubectl create configmap kafka-brokers --from-literal=brokers="my-cluster-kafka-bootstrap.kafka.svc" -n shipping
+:: This uses port 9093 (with TLS)
+kubectl create configmap kafka-brokers --from-literal=brokers="my-cluster-kafka-bootstrap.kafka.svc:9093" -n shipping
 :: or: kubectl apply -f kafka-brokers.yaml
 
 :: Create Kafka topics
 kubectl apply -f topics.yaml
+
+:: TODO - make sure other repos are cloned
+
+:: Install order-command-ms
+:: note: Using ibmcase/kcontainer-order-command-ms pre-built image
+:: note: --set eventstreams.enabled=false for connecting to Kafka directly
+helm install order-command-ms ../refarch-kc-order-ms/order-command-ms/chart/ordercommandms --wait --timeout=300s --set image.repository=ibmcase/kcontainer-order-command-ms --set kafka.brokersConfigMap=kafka-brokers --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount --namespace shipping
 
 :: call postinstall.bat
