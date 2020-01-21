26784862a37192aceb2c71739bf6371dd239ac0c
David Jones <djones6@uk.ibm.com>
2020-01-21 14:22:28 +0000

    Add install for first microservice, fixes

--- uninstall.bat
+++ uninstall.bat
@@ -1,10 +1,28 @@
-:: Delete shipping namespace
-kubectl delete ns shipping
+:: Uninstall order-command-ms
+helm uninstall order-command-ms -n shipping
+
+:: Remove Kafka topics
+kubectl delete -f topics.yaml
+
+:: Delete Postgres secrets
+kubectl delete secret postgresql-url -n shipping
+kubectl delete secret postgresql-user -n shipping
+kubectl delete secret postgresql-pwd -n shipping
+
+:: Delete Kafka configmap
+kubectl delete configmap kafka-brokers -n shipping
 
 :: Uninstall Postgres Helm chart
 helm uninstall postgresql -n postgres
 
-:: Delete namespace for Postgres
+:: TODO - is this required outside of OpenShift?
+kubectl delete serviceaccount -n postgres pgserviceaccount
+kubectl delete serviceaccount -n shipping kcserviceaccount
+
+:: Delete refarch-kc microservices namespace
+kubectl delete ns shipping
+
+:: Delete Postgres namespace
 kubectl delete ns postgres
 
 :: Delete Kafka cluster
@@ -13,6 +31,6 @@ kubectl delete -f kafka-strimzi.yml -n kafka
 :: Uninstall Strimzi Helm chart
 helm uninstall strimzi -n strimzi
 
-:: Delete namespaces for Strimzi and Kafka
+:: Remove Strimzi and Kafka namespaces
 kubectl delete ns strimzi
 kubectl delete ns kafka
