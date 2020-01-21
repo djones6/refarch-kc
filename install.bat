0c08393861ae05d8765bf7e0d0fbe63af13e9226
David Jones <djones6@uk.ibm.com>
2020-01-21 17:19:44 +0000

    Install order-query-ms and spring-container-ms

--- install.bat
+++ install.bat
@@ -25,13 +25,13 @@ helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm install postgresql stable/postgresql -n postgres --set postgresqlPassword=supersecret --set persistence.enabled=false --set serviceAccount.enabled=true --set serviceAccount.name=pgserviceaccount
 
 :: Configure secrets to allow microservices to connect to Postgres
-kubectl create secret generic postgresql-url --from-literal=binding="jdbc:postgresql://postgresql.postgres.svc" -n shipping
+kubectl create secret generic postgresql-url --from-literal=binding="jdbc:postgresql://postgresql.postgres.svc:5432/" -n shipping
 kubectl create secret generic postgresql-user --from-literal=binding="postgres" -n shipping
 kubectl create secret generic postgresql-pwd --from-literal=binding="supersecret" -n shipping
 
 :: Create a configmap to allow microservices to find Kafka
 :: This uses port 9093 (with TLS)
-kubectl create configmap kafka-brokers --from-literal=brokers="my-cluster-kafka-bootstrap.kafka.svc:9093" -n shipping
+kubectl create configmap kafka-brokers --from-literal=brokers="my-cluster-kafka-bootstrap.kafka.svc:9092" -n shipping
 :: or: kubectl apply -f kafka-brokers.yaml
 
 :: Create Kafka topics
@@ -39,9 +39,16 @@ kubectl apply -f topics.yaml
 
 :: TODO - make sure other repos are cloned
 
-:: Install order-command-ms
-:: note: Using ibmcase/kcontainer-order-command-ms pre-built image
-:: note: --set eventstreams.enabled=false for connecting to Kafka directly
+:: Install order-command-ms using pre-built image
+:: note: --set eventstreams.enabled=false and --set eventstreams.truststoreRequired=false 
+:: for connecting to Kafka rather than Event Streams
 helm install order-command-ms ../refarch-kc-order-ms/order-command-ms/chart/ordercommandms --wait --timeout=300s --set image.repository=ibmcase/kcontainer-order-command-ms --set kafka.brokersConfigMap=kafka-brokers --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount --namespace shipping
+
+:: Install order-query-ms using pre-built image
+helm install order-query-ms ../refarch-kc-order-ms/order-query-ms/chart/orderqueryms --wait --timeout=300s --set image.repository=ibmcase/kcontainer-order-query-ms --set kafka.brokersConfigMap=kafka-brokers --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount --namespace shipping
+
+:: Install spring-container-ms using pre-built image
+:: note: uses postgres secrets defined above
+helm install spring-container-ms ..\refarch-kc-container-ms\SpringContainerMS\chart\springcontainerms --set image.repository=ibmcase/kcontainer-spring-container-ms --set kafka.brokersConfigMap=kafka-brokers --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount --namespace shipping
 
 :: call postinstall.bat
