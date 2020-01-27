193509cad8fb898a9118588ea6ee72660d172b55
David Jones <djones6@uk.ibm.com>
2020-01-27 16:53:23 +0000

    Add equivalent shell scripts

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,8 +1,10 @@
-:: Deploy integration tests job
+#!/bin/sh
+
+# Deploy integration tests job
 kubectl apply -f itg-tests/es-it/ReeferItgTests.yaml -n shipping
 
-:: Wait for job pod to be ready (ie. job has started running)
+# Wait for job pod to be ready (ie. job has started running)
 kubectl wait --for=condition=ready pod -l job-name=reefer-itgtests-job -n shipping
 
-:: Follow the job log
+# Follow the job log
 kubectl logs job/reefer-itgtests-job -n shipping -f
---
+++ uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+./uninstall-app.sh
+
+# Uninstall Postgres Helm chart
+helm uninstall postgresql -n postgres
+
+# TODO - is this required outside of OpenShift?
+kubectl delete serviceaccount -n postgres pgserviceaccount
+
+# Delete Postgres namespace
+kubectl delete ns postgres
+
+# Delete Kafka cluster
+kubectl delete -f kafka-strimzi.yml -n kafka
+
+# Uninstall Strimzi Helm chart
+helm uninstall strimzi -n strimzi
+
+# Remove Strimzi and Kafka namespaces
+kubectl delete ns strimzi
+kubectl delete ns kafka
