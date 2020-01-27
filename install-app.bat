0259f7da03e1e3e80ca362fcf3ee096af04f2ba5
David Jones <djones6@uk.ibm.com>
2020-01-27 11:08:06 +0000

    Update scripts

--- install-app.bat
+++ install-app.bat
@@ -5,7 +5,7 @@ kubectl create ns shipping
 kubectl create serviceaccount -n shipping kcserviceaccount
 
 :: Configure secrets to allow microservices to connect to Postgres
-kubectl create secret generic postgresql-url --from-literal=binding="jdbc:postgresql://postgresql.postgres.svc:5432/" -n shipping
+kubectl create secret generic postgresql-url --from-literal=binding="jdbc:postgresql://postgresql.postgres.svc:5432/postgres" -n shipping
 kubectl create secret generic postgresql-user --from-literal=binding="postgres" -n shipping
 kubectl create secret generic postgresql-pwd --from-literal=binding="supersecret" -n shipping
 
@@ -22,22 +22,30 @@ kubectl apply -f topics.yaml
 :: Install order-command-ms using pre-built image
 :: note: --set eventstreams.enabled=false and --set eventstreams.truststoreRequired=false 
 :: for connecting to Kafka rather than Event Streams
-helm install order-command-ms ../refarch-kc-order-ms/order-command-ms/chart/ordercommandms -n shipping --wait --timeout=300s --set image.repository=ibmcase/kcontainer-order-command-ms --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount
+helm install order-command-ms ../refarch-kc-order-ms/order-command-ms/chart/ordercommandms -n shipping --set image.repository=ibmcase/kcontainer-order-command-ms --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount
 
 :: Install order-query-ms using pre-built image
-helm install order-query-ms ../refarch-kc-order-ms/order-query-ms/chart/orderqueryms -n shipping --wait --timeout=300s --set image.repository=ibmcase/kcontainer-order-query-ms --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount
+helm install order-query-ms ../refarch-kc-order-ms/order-query-ms/chart/orderqueryms -n shipping --set image.repository=ibmcase/kcontainer-order-query-ms --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount
 
 :: Install spring-container-ms using pre-built image
 :: note: uses postgres secrets defined above
-helm install spring-container-ms ../refarch-kc-container-ms/SpringContainerMS/chart/springcontainerms -n shipping --wait --timeout=300s --set image.repository=ibmcase/kcontainer-spring-container-ms --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount
+helm install spring-container-ms ../refarch-kc-container-ms/SpringContainerMS/chart/springcontainerms -n shipping --set image.repository=ibmcase/kcontainer-spring-container-ms --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount
 
 :: Install voyages-ms using pre-built image
-helm install voyages-ms ../refarch-kc-ms/voyages-ms/chart/voyagesms --set image.repository=ibmcase/kcontainer-voyages-ms -n shipping --wait --timeout=300s --set eventstreams.enabled=false --set serviceAccountName=kcserviceaccount
+helm install voyages-ms ../refarch-kc-ms/voyages-ms/chart/voyagesms --set image.repository=ibmcase/kcontainer-voyages-ms -n shipping --set eventstreams.enabled=false --set serviceAccountName=kcserviceaccount
 
 :: Install fleet-ms
-helm install fleet-ms ../refarch-kc-ms/fleet-ms/chart/fleetms --set image.repository=ibmcase/kcontainer-fleet-ms -n shipping --wait --timeout=300s --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount
+helm install fleet-ms ../refarch-kc-ms/fleet-ms/chart/fleetms --set image.repository=ibmcase/kcontainer-fleet-ms -n shipping --set eventstreams.enabled=false --set eventstreams.truststoreRequired=false --set serviceAccountName=kcserviceaccount
 
 :: Install kc-ui
-helm install kc-ui ../refarch-kc-ui/chart/kc-ui --set image.repository=ibmcase/kcontainer-ui -n shipping --wait --timeout=300s --set eventstreams.enabled=false --set serviceAccountName=kcserviceaccount
+helm install kc-ui ../refarch-kc-ui/chart/kc-ui --set image.repository=ibmcase/kcontainer-ui -n shipping --set eventstreams.enabled=false --set serviceAccountName=kcserviceaccount
+
+:: Wait for all services to start
+kubectl wait -n shipping --for=condition=available service springcontainerms-service
+kubectl wait -n shipping --for=condition=available service fleetms-service
+kubectl wait -n shipping --for=condition=available service kc-ui-service
+kubectl wait -n shipping --for=condition=available service ordercommandms-service
+kubectl wait -n shipping --for=condition=available service orderqueryms-service
+kubectl wait -n shipping --for=condition=available service voyagesms-service
 
 :: call postinstall.bat
--- install.bat
+++ install.bat
@@ -1,24 +1,3 @@
-:: Create namespaces for Strimzi and Kafka
-kubectl create ns strimzi
-kubectl create ns kafka
-
-:: Install Strimzi Helm chart
-helm repo add strimzi https://strimzi.io/charts
-helm install strimzi strimzi/strimzi-kafka-operator -n strimzi --set watchNamespaces={kafka} --wait --timeout 300s
-
-:: Install Strimzi custom resource, and wait for cluster creation
-kubectl apply -f kafka-strimzi.yml -n kafka
-kubectl wait --for=condition=Ready kafkas/my-cluster -n kafka --timeout 180s
-
-:: Create namespace for Postgres
-kubectl create ns postgres
-
-:: TODO - is this required outside of OpenShift?
-kubectl create serviceaccount -n postgres pgserviceaccount
-
-:: Install Postgres Helm chart
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
-helm install postgresql stable/postgresql -n postgres --wait --timeout=300s --set postgresqlPassword=supersecret --set persistence.enabled=false --set serviceAccount.enabled=true --set serviceAccount.name=pgserviceaccount
-
+call install-infra.bat
 call install-app.bat
 call run-integration-tests.bat
