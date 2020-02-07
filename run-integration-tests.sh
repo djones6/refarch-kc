b058a37178707ec5dbc04404d146df6e6fecc3c0
David Jones <djones6@uk.ibm.com>
2020-02-07 10:37:34 +0000

    Increase wait for integration tests to tolerate first image pull delay

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -4,7 +4,8 @@
 kubectl apply -f itg-tests/es-it/ReeferItgTests.yaml -n shipping
 
 # Wait for job pod to be ready (ie. job has started running)
-kubectl wait --for=condition=ready pod -l job-name=reefer-itgtests-job -n shipping
+# This will take seconds, unless the image needs pulling from a remote registry.
+kubectl wait --for=condition=ready pod -l job-name=reefer-itgtests-job -n shipping --timeout 180s
 
 # Follow the job log
 kubectl logs job/reefer-itgtests-job -n shipping -f
