a3617ca0980fc5ff6e39bc4701cbc39dcea57dd2
David Jones <djones6@uk.ibm.com>
2020-02-07 10:37:01 +0000

    Improve infrastructure install time

--- install-infra.sh
+++ install-infra.sh
@@ -8,9 +8,8 @@ kubectl create ns kafka
 helm repo add strimzi https://strimzi.io/charts
 helm install strimzi strimzi/strimzi-kafka-operator -n strimzi --set watchNamespaces={kafka} --wait --timeout 300s
 
-# Install Strimzi custom resource, and wait for cluster creation
+# Install Strimzi custom resource to create cluster
 kubectl apply -f kafka-strimzi.yml -n kafka
-kubectl wait --for=condition=Ready kafkas/my-cluster -n kafka --timeout 180s
 
 # Create namespace for Postgres
 kubectl create ns postgres
@@ -19,5 +18,8 @@ kubectl create ns postgres
 kubectl create serviceaccount -n postgres pgserviceaccount
 
 # Install Postgres Helm chart
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
-helm install postgresql stable/postgresql -n postgres --wait --timeout=300s --set postgresqlPassword=supersecret --set persistence.enabled=false --set serviceAccount.enabled=true --set serviceAccount.name=pgserviceaccount
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install postgresql bitnami/postgresql -n postgres --wait --timeout=300s --set postgresqlPassword=supersecret --set persistence.enabled=false --set serviceAccount.enabled=true --set serviceAccount.name=pgserviceaccount
+
+# Wait for cluster to be ready before continuing
+kubectl wait --for=condition=Ready kafkas/my-cluster -n kafka --timeout 180s
